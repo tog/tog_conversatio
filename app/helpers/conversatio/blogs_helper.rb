eb8ac488e6e621d5eb42d90c0af2a46c36064253
Roberto Salicio <roberto.salicio@gmail.com>
2008-09-24 18:04:26 +0200

    Ticket #83. Cant's show all blogs' tag cloud

--- app/helpers/conversatio/blogs_helper.rb
+++ app/helpers/conversatio/blogs_helper.rb
@@ -1,9 +1,9 @@
 module Conversatio
   module BlogsHelper
-    
+
     def last_posts(blog, limit=5)
       blog.published_posts[0..limit-1]
-    end    
+    end
 
     def archives_menu
       html = "<ul>"
@@ -13,14 +13,14 @@ module Conversatio
         if @year
           html << "<ul>"
           for m in 1..12
-            html << "<li>#{link_to(Date::ABBR_MONTHNAMES[m], monthly_archives_conversatio_blog_posts_path(@blog, year, m))}</li>" 
+            html << "<li>#{link_to(Date::ABBR_MONTHNAMES[m], monthly_archives_conversatio_blog_posts_path(@blog, year, m))}</li>"
           end
           html << "</ul>"
         end
       end
       html << "</ul>"
     end
-    
+
     def tag_cloud_posts(classes)
       return if !@blog
       tags = @blog.posts.tag_counts(:conditions => ["posts.state='published'"])
@@ -31,7 +31,7 @@ module Conversatio
         yield tag, classes[index]
       end
     end
-    
+
     def tag_cloud_blogs(classes)
       tags = Conversatio::Post.tag_counts(:conditions => ["posts.state='published'"])
       return if tags.empty?
@@ -41,6 +41,6 @@ module Conversatio
         yield tag, classes[index]
       end
     end
-    
+
   end
 end
