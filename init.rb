8b22bc0bd883789cbee2ccc77736b20a72019783
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-02-06 19:34:03 +0100

    i18n for navigation tabs

--- init.rb
+++ init.rb
@@ -2,6 +2,11 @@ require_plugin 'tog_core'
 require_plugin 'seo_urls'
 require_plugin 'thinking-sphinx'
 
+require "i18n" unless defined?(I18n)
+Dir[File.dirname(__FILE__) + '/locale/**/*.yml'].each do |file|
+  I18n.load_path << file
+end
+
 Tog::Interface.sections(:site).add "Blogs", "/blogs"
 
 Tog::Interface.sections(:member).add "Blogs", "/member/conversatio/blogs"
