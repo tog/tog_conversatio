50ffaaeb99636cbf19a83eb036a890d7d7c7159e
Andrei Erdoss <erdoss@gmail.com>
2009-06-18 22:03:41 +0800

    Destroy posts when deleting blog
    Signed-off-by: Alberto Molpeceres <alberto.molpeceres@gmail.com>

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -15,8 +15,7 @@ class Blog < ActiveRecord::Base
   
   acts_as_taggable
   
-  has_many   :posts
-
+  has_many   :posts,           :dependent => :destroy
   has_many   :bloggerships,    :dependent => :destroy
   has_many   :users,           :through => :bloggerships
   belongs_to :author,          :class_name => "User", :foreign_key => "author_id"
