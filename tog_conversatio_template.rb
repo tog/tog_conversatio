649834987edb922eca4ccc9ce85247d3fa6b5e16
molpe <alberto.molpeceres@gmail.com>
2009-10-13 22:14:27 +0200

    use update_tog_migration migration instead of hard-coded migration

--- tog_conversatio_template.rb
+++ tog_conversatio_template.rb
@@ -12,17 +12,7 @@ plugin 'tog_conversatio', :git => "git://github.com/tog/tog_conversatio.git"
 
 route "map.routes_from_plugin 'tog_conversatio'"
 
-file "db/migrate/" + Time.now.strftime("%Y%m%d%H%M%S") + "_install_tog_conversatio.rb",
-%q{class InstallTogConversatio < ActiveRecord::Migration
-    def self.up
-      migrate_plugin "tog_conversatio", 5
-    end
-
-    def self.down
-      migrate_plugin "tog_conversatio", 0 
-    end
-end
-}
+generate "update_tog_migration"
 
 if yes?("Generate sphinx index? (only for MySQL and PostgreSQL) (y/n)")
   rake "thinking_sphinx:index"
